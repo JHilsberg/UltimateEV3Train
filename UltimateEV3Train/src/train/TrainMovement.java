4f921e2c4b37b57e15a8d9cc50a31664eb20fbe6
Julian_Thinkpad <Julian@141.46.221.197>
2014-05-08 14:24:46 +0200

    Train Movement: added Unload and Load

--- UltimateEV3Train/src/train/TrainMovement.java
+++ UltimateEV3Train/src/train/TrainMovement.java
@@ -5,25 +5,40 @@ import lejos.hardware.port.MotorPort;
 import lejos.robotics.RegulatedMotor;
 
 public class TrainMovement {
-	
+
 	private RegulatedMotor movingMotor = new EV3LargeRegulatedMotor(MotorPort.A);
-	private RegulatedMotor loadingMotor = new EV3MediumRegulatedMotor(MotorPort.D);
-	
+	private RegulatedMotor loadingMotor = new EV3MediumRegulatedMotor(
+			MotorPort.D);
+
 	private int movingSpeed = 300;
 	private int loadingSpeed = 40;
-	
-	public void forward(){
+
+	public void forward() {
 		this.movingMotor.setSpeed(this.movingSpeed);
 		this.movingMotor.backward();
 	}
-	
-	public void stop(){
+
+	public void stop() {
 		this.movingMotor.stop();
 		this.movingMotor.flt();
 	}
-	
-	public void unload(){
+
+	public void unload() {
+		int loadAngle = 40;
 		this.loadingMotor.setSpeed(this.loadingSpeed);
-		this.loadingMotor.forward();
+
+		while (this.loadingMotor.getTachoCount() < loadAngle) {
+			this.loadingMotor.forward();
+		}
+		this.loadingMotor.stop();
+	}
+
+	public void load() {
+		this.loadingMotor.setSpeed(this.loadingSpeed);
+
+		while (this.loadingMotor.getTachoCount() > 0) {
+			this.loadingMotor.backward();
+		}
+		this.loadingMotor.stop();
 	}
 }
