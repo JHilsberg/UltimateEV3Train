5a62d50169af5f3719a97438defe901a0b69465c
Hannes <Hannes@141.46.221.115>
2014-05-08 13:17:47 +0200

    permanently detecting colors

---
+++ UltimateEV3Train/src/train/ColorDetection.java
@@ -0,0 +1,27 @@
+package train;
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.*;
+import lejos.hardware.sensor.*;
+
+public class ColorDetection {
+	
+	private EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S1);
+	private int detectedColor;
+	
+	public ColorDetection(){
+		while(Button.ESCAPE.isDown() == false){
+			detectedColor = colorSensor.getColorID();
+			LCD.drawString("erkannte Farbe: " + detectedColor, 0, 0);
+		}
+	}
+	
+	public int getColor() {
+		return detectedColor;
+	}
+	public void setColor(int color) {
+		this.detectedColor = color;
+	}
+
+}
