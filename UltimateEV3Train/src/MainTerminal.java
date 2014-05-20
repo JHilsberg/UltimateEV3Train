85ed7e6da89c20075a9582637cc15b532abbd7f1
Gelenkesteffen <s3jeklin@stud.hszg.de>
2014-05-20 12:40:06 +0200

    -Ansteuerung loadLeft und terminalReset läuft

--- UltimateEV3Train/src/MainTerminal.java
+++ UltimateEV3Train/src/MainTerminal.java
@@ -10,16 +10,40 @@ public class MainTerminal {
 
 	MainTerminal() {
 		TerminalControl terminal = new TerminalControl();
-		 LCD.drawInt(terminal.tachoElevatorLeft, 0, 0);
-		 LCD.drawInt(terminal.tachoElevatorRight, 0, 1);
-		 LCD.drawInt(terminal.tachoRotationMotor, 0, 2);
-		 LCD.refresh();
-		while (Button.ESCAPE.isDown() == false) {
-			//terminal.loadTerminal(true, false);
-			// terminal.unloadTerminal(true, false);
-			 terminal.loadTerminal(false, true);
-			// terminal.unloadTerminal(false, true);
-			
+		terminal.loadTerminalLeft();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Automatisch generierter Erfassungsblock
+			e.printStackTrace();
 		}
+		terminal.resetTerminal();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Automatisch generierter Erfassungsblock
+			e.printStackTrace();
+		}
+		terminal.loadTerminalRight();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Automatisch generierter Erfassungsblock
+			e.printStackTrace();
+		}
+		terminal.resetTerminal();
+		
+		
+		while (Button.ESCAPE.isDown() == false) {
+			LCD.drawString("Links: ", 0, 0);
+			LCD.drawInt(terminal.getElevatorLeft().getTachoCount(), 10, 0);
+			LCD.drawString("Rechts: ", 0, 1);
+			LCD.drawInt(terminal.getElevatorRight().getTachoCount(), 10, 1);
+			LCD.drawString("Rotation: " + terminal.getRotationMotor().getTachoCount(), 0, 2);
+			
+			// terminal.unloadTerminal(true, false);
+			// terminal.loadTerminal(false, true);
+			// terminal.unloadTerminal(false, true);
+					}
 	}
 }
