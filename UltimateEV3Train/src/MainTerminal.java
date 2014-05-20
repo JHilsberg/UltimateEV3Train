47d7262f34d06e29020f7762d21990b6f7df92ac
Julian_Thinkpad <Julian>
2014-05-20 12:52:29 +0200

    ...

--- UltimateEV3Train/src/MainTerminal.java
+++ UltimateEV3Train/src/MainTerminal.java
@@ -10,40 +10,16 @@ public class MainTerminal {
 
 	MainTerminal() {
 		TerminalControl terminal = new TerminalControl();
-		terminal.loadTerminalLeft();
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Automatisch generierter Erfassungsblock
-			e.printStackTrace();
-		}
-		terminal.resetTerminal();
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Automatisch generierter Erfassungsblock
-			e.printStackTrace();
-		}
 		terminal.loadTerminalRight();
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Automatisch generierter Erfassungsblock
-			e.printStackTrace();
-		}
-		terminal.resetTerminal();
-		
-		
+
+
 		while (Button.ESCAPE.isDown() == false) {
 			LCD.drawString("Links: ", 0, 0);
 			LCD.drawInt(terminal.getElevatorLeft().getTachoCount(), 10, 0);
 			LCD.drawString("Rechts: ", 0, 1);
 			LCD.drawInt(terminal.getElevatorRight().getTachoCount(), 10, 1);
-			LCD.drawString("Rotation: " + terminal.getRotationMotor().getTachoCount(), 0, 2);
-			
-			// terminal.unloadTerminal(true, false);
-			// terminal.loadTerminal(false, true);
-			// terminal.unloadTerminal(false, true);
-					}
+			LCD.drawString("Rotation: "
+					+ terminal.getRotationMotor().getTachoCount(), 0, 2);
+			}
 	}
 }
